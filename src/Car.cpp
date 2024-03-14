46b53676f4a5d47d31ff8164bd845670ab627be9
Alexander74 <alexandre.lanternier@outlook.fr>
2024-03-14 16:17:32 +0100

    ajout de la class car

--- src/Car.cpp
+++ src/Car.cpp
@@ -2,6 +2,8 @@
 #include <BBOP/Graphics/bbopMathClass.h>
 #include <string>
 
+#include <iostream>
+
 Car::Car(std::string new_fold_name, Vector2f new_position, float new_rotation, float new_max_speed)
   : fold_name(new_fold_name),
     pos(new_position),
@@ -12,16 +14,19 @@ Car::Car(std::string new_fold_name, Vector2f new_position, float new_rotation, f
 {
   int imgn = 100;
   for(int i = 0; i < 4 ; i++){
-    imgn = imgn/i;
-    std::string filename = fold_name + "/" + std::to_string(imgn) + ".png";
+    if(i == 3)
+      imgn = 0;
+    std::string filename = fold_name + "/" + std::to_string(imgn) + "/0.png";
+    std::cout << filename << std::endl;
     Sprite toadd(Texture(filename.c_str()), pos);
     toadd.setSize(Vector2f(50.0f,50.0f));
     toadd.setOrigin(Vector2f(toadd.getSize().x/2.0f,toadd.getSize().y/2.0f));
     sprites.push_back(toadd);
+    imgn = imgn/2;
   }
 }
 
-void Draw(GLint renderModLoc) const
+void Car::Draw(GLint renderModLoc) const
 {
   sprites[state].Draw(renderModLoc);
 }
