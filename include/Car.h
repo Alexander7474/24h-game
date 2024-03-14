af5a9ee8fbe16f0842316d551c5830c7400c7f5c
Alexander74 <alexandre.lanternier@outlook.fr>
2024-03-14 15:50:23 +0100

    ajout de la class car

--- include/Car.h
+++ include/Car.h
@@ -1,10 +1,34 @@
-#ifndef CAR_CLASS_H
+#pragma once
 
+#include <BBOP/Graphics/bbopMathClass.h>
+#include <string>
+#include <vector>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <BBOP/Graphics.h>
 
 class Car : public BbopDrawable
+{
+public:
+  Car(std::string new_fold_name, Vector2f new_pos, float new_rotation, float new_max_speed);
 
-#endif // !CAR_CLASS_H
+  void accelerate();
+  void decelerate();
+  void go_left();
+  void go_right();
+  void set_pos();
+  Vector2f get_pos();
+  void set_rotation();
+  float get_rotation();
 
+  void Draw(GLint renderModLoc) const override;
+private: 
+  std::vector<Sprite> sprites;
+  Vector2f pos;
+  float rotation;
+  Vector2f speed;
+  float max_speed;
+  float max_speed_reverse;
+  std::string fold_name;
+  int state;
+};
---
+++ src/Car.cpp
@@ -0,0 +1,27 @@
+#include "../include/Car.h"
+#include <BBOP/Graphics/bbopMathClass.h>
+#include <string>
+
+Car::Car(std::string new_fold_name, Vector2f new_position, float new_rotation, float new_max_speed)
+  : fold_name(new_fold_name),
+    pos(new_position),
+    rotation(new_rotation),
+    max_speed(new_max_speed),
+    max_speed_reverse(new_max_speed),
+    state(0)
+{
+  int imgn = 100;
+  for(int i = 0; i < 4 ; i++){
+    imgn = imgn/i;
+    std::string filename = fold_name + "/" + std::to_string(imgn) + ".png";
+    Sprite toadd(Texture(filename.c_str()), pos);
+    toadd.setSize(Vector2f(50.0f,50.0f));
+    toadd.setOrigin(Vector2f(toadd.getSize().x/2.0f,toadd.getSize().y/2.0f));
+    sprites.push_back(toadd);
+  }
+}
+
+void Draw(GLint renderModLoc) const
+{
+  sprites[state].Draw(renderModLoc);
+}
