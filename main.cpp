c4efa01e6bf7fa33c040645ba3144562e42d8c4d
Alexander74 <alexandre.lanternier@outlook.fr>
2024-03-14 14:27:03 +0100

    Base du jeu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 /////////////////////////////
 //GAME SKELETON
 /////////////////////////////
-#include <BBOP/Graphics/bbopFunc.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <BBOP/Graphics.h>
