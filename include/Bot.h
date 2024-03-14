072892081c22a797d49cd142432bbdcc5a59ab52
Alexander74 <alexandre.lanternier@outlook.fr>
2024-03-14 14:26:41 +0100

    Base du jeu

---
+++ include/Bot.h
@@ -0,0 +1,4 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <BBOP/Graphics.h>
+
---
+++ include/Car.h
@@ -0,0 +1,10 @@
+#ifndef CAR_CLASS_H
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <BBOP/Graphics.h>
+
+class Car : public BbopDrawable
+
+#endif // !CAR_CLASS_H
+
