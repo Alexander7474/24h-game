f56e5ee65c275ebd8501afea6d37af0c28d76860
NetleyOnVibe <129487453+NetleyOnVibe@users.noreply.github.com>
2024-03-15 10:48:44 +0100

    map avec pneu

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,7 +1,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <BBOP/Graphics.h>
-#include <irrKlang/irrKlang.h>
 #include <vector>
 #include <cstdlib>
 #include <ctime>
@@ -17,10 +16,17 @@ Obstacle::Obstacle()
         std::srand(time(NULL));
         initialized = true;
     }
+
     random_pos_x = rand() % (BBOP_WINDOW_SIZE.x - offsetx*2) + offsetx;
     random_pos_y = rand() % (BBOP_WINDOW_SIZE.y - offsety*2) + offsety;
     obs.setSize(Vector2f(50,50));
     obs.setPosition(Vector2f(random_pos_x, random_pos_y));
+    for(int i = 0;i<7; i++)
+    {
+        std::string pneu_obst = "img/map/obstacles/pneu/" + std::to_string(i) + ".png";
+        Texture pneu_ob(pneu_obst.c_str());
+        list_pneu.push_back(pneu_ob);
+    }
 }
 
 
@@ -40,12 +46,35 @@ Map::Map()
     }
 
     map_s.setSize(Vector2f(BBOP_WINDOW_SIZE.x, BBOP_WINDOW_SIZE.y));
+
+
+
+
 }
 
 std::vector<Obstacle> Map::get_obst()
 {
     return list_obst;
 }
+
+
+void Obstacle::update()
+{
+    anime_frame ++;
+    if (anime_frame >= list_pneu.size()*15) anime_frame = 0;
+    obs.setTexture(list_pneu[anime_frame/15]);
+}
+
+
+void Map::update()
+{
+    for(int i = 0; i < list_obst.size(); i++)
+    {
+        list_obst[i].update();
+    }
+}
+
+
 
 void Obstacle::Draw(GLint renderModeLoc) const
 {
