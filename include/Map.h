1dfa32e9711fa9212ca13a3f21ba2aff90357de3
NetleyOnVibe <129487453+NetleyOnVibe@users.noreply.github.com>
2024-03-15 10:49:35 +0100

    map avec pneu

--- include/Map.h
+++ include/Map.h
@@ -12,26 +12,20 @@ class Obstacle:public BbopDrawable
 {
 private:
     Sprite obs;
+    std::vector<Texture>list_pneu;
     int x;
     int y;
     int offsetx = 100 ;
     int offsety = 75 ;
     int random_pos_x;
     int random_pos_y;
+    int anime_frame;
 public:
     Obstacle();
     Sprite *get_sprite();
     virtual void Draw(GLint renderModeLoc) const override;
+    void update();
 
-
-};
-
-
-class Box:public BbopDrawable
-{
-public:
-    Box();
-    virtual void Draw(GLint renderModeLoc) const override;
 
 };
 
@@ -45,6 +39,8 @@ public:
     Map();
     std::vector<Obstacle>get_obst();
     virtual void Draw(GLint renderModeLoc) const override;
+    void update();
+    
 
     
     
